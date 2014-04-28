36835653da195e19933dce8379d9ac75642d0825
Jacob Friedman <jacob@subsumo.com>
2014-04-28 15:39:39 +0200

    yo scaffold (angular, compass)

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular
+  .module('carbonmapApp', [
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
